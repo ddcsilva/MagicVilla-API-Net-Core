4c4f5a3732b404828c871dcd7c0cc43325f23430
Danilo <danilo.silva@msn.com>
2023-07-06 00:30:01 -0300

    Adicionando DataAnnotations

--- src/MagicVilla.API/Models/DTO/VilaDTO.cs
+++ src/MagicVilla.API/Models/DTO/VilaDTO.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MagicVilla.API.Models.DTO;
 
 public class VilaDTO
 {
     public int Id { get; set; }
+    
+    [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+    [MaxLength(50, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres.")]
     public string Nome { get; set; }
 }
