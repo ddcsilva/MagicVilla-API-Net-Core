5c672270f31620f416e3c302d2caf7a7eafb37df
Danilo <danilo.silva@msn.com>
2023-07-06 00:30:30 -0300

    Documentando API e Validações Customizadas na ModelState

--- src/MagicVilla.API/Controllers/VilasController.cs
+++ src/MagicVilla.API/Controllers/VilasController.cs
@@ -9,14 +9,46 @@ namespace MagicVilla.API.Controllers;
 public class VilasController : ControllerBase
 {
     [HttpGet]
-    public IEnumerable<VilaDTO> ObterVilas()
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult<IEnumerable<VilaDTO>> ObterVilas()
     {
-        return MagicVillaStore.listaVilas;
+        return Ok(MagicVillaStore.listaVilas);
     }
 
-    [HttpGet("{id}")]
-    public VilaDTO ObterVila(int id)
+    [HttpGet("{id:int}", Name = "ObterVila")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<VilaDTO> ObterVila(int id)
     {
-        return MagicVillaStore.listaVilas.FirstOrDefault(v => v.Id == id);
+        if (id <= 0) return BadRequest("Id inválido");
+
+        var vila = MagicVillaStore.listaVilas.FirstOrDefault(v => v.Id == id);
+
+        if (vila == null) return NotFound();
+
+        return Ok();
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public ActionResult<VilaDTO> AdicionarVila([FromBody] VilaDTO vilaDTO)
+    {
+        if (vilaDTO == null) return BadRequest("Vila inválida");
+
+        if (vilaDTO.Id > 0) return StatusCode(StatusCodes.Status500InternalServerError);
+
+        if (MagicVillaStore.listaVilas.Any(v => v.Nome == vilaDTO.Nome)) 
+        {
+            ModelState.AddModelError("Nome", "Vila já cadastrada");
+            return BadRequest(ModelState);
+        }
+
+        vilaDTO.Id = MagicVillaStore.listaVilas.Max(v => v.Id) + 1;
+        MagicVillaStore.listaVilas.Add(vilaDTO);
+
+        return CreatedAtRoute("ObterVila", new { id = vilaDTO.Id }, vilaDTO);
     }
 }
